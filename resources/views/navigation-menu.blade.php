7d71d2c9432f70dc4a053aef13722db9b09fb44f
kolaente <k@knt.li>
2021-10-03 13:59:10 +0200

    fix: menu highlight for current item

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,7 +15,7 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('personal-data.edit') }}" :active="request()->routeIs('dashboard')">
+                    <x-jet-nav-link href="{{ route('personal-data.edit') }}" :active="request()->routeIs('personal-data.edit')">
                         {{ __('Personal Data') }}
                     </x-jet-nav-link>
                 </div>
