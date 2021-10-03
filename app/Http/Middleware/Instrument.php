d2410566b643107e135e293c2ce235d683b49e3b
kolaente <k@knt.li>
2021-10-03 13:51:24 +0200

    feat: add must set instrument middleware

---
+++ app/Http/Middleware/Instrument.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Instrument
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+
+        if($user->personalData->instrument === null) {
+            return redirect(route('set-instrument.form'));
+        }
+
+        return $next($request);
+    }
+}
