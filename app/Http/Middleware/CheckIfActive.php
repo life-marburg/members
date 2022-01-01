b15b6bf03004e0aea3f4bcbdbae713417842d484
kolaente <k@knt.li>
2022-01-01 22:16:33 +0100

    feat: add not yet active middleware

---
+++ app/Http/Middleware/CheckIfActive.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckIfActive
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::user()->status !== User::STATUS_UNLOCKED) {
+            return redirect(route('not-yet-active'));
+        }
+
+        return $next($request);
+    }
+}
