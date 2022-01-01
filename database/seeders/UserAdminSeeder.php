6b46cee57158c4822e623bfb83e4d559949ef877
kolaente <k@knt.li>
2022-01-01 17:13:12 +0100

    chore: add admin user seeder to modify permissions

---
+++ database/seeders/UserAdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Rights;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class UserAdminSeeder extends Seeder
+{
+    // Makes the first user admin
+    public function run()
+    {
+        $user = User::whereId(1)->firstOrFail();
+        $user->assignRole(Rights::R_ADMIN);
+
+        DB::table('users')->update(['status' => User::STATUS_UNLOCKED]);
+    }
+}
