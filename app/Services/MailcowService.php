fe9add27b8ec43a09221485e4454eb1b9db93a36
kolaente <k@knt.li>
2022-01-02 20:52:11 +0100

    feat: add empty mailcow service and config file

---
+++ app/Services/MailcowService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+class MailcowService
+{
+    const PUBLIC_EMAIL_ALIAS_ID = null;
+
+    public function getEmailAddressesInAlias()
+    {
+
+    }
+}
---
+++ config/mailcow.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'path' => 'https://mail.kolaente.de/',
+    'api_key' => env('MAILCOW_API_KEY'),
+];
