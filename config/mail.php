aa499fb1914622dcdd5f873111b20fa2625e3418
kolaente <k@knt.li>
2022-01-02 18:01:41 +0100

    fix: set default from address

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => 'info@life-marburg.de',
+        'name' => 'Life Marburg',
     ],
 
     /*
