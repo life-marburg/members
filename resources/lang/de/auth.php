cf72754ea6a94b5d5665c4f6b8c06337def8abaf
kolaente <k@knt.li>
2022-01-07 18:47:29 +0100

    feat: add translations

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Authentication Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used during authentication for various
+| messages that we need to display to the user. You are free to modify
+| these language lines according to your application's requirements.
+|
+*/
+
+return [
+    'failed'   => 'Diese Kombination aus Zugangsdaten kennen wir leider nicht.',
+    'password' => 'Das eingegebene Passwort ist falsch.',
+    'throttle' => 'Zu viele Loginversuche. Versuche es bitte in :seconds Sekunden nochmal.',
+];
