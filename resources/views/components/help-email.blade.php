cc5cb4f01e3002b2e106ecbcb668d513ad0b9ab1
kolaente <k@knt.li>
2022-01-07 21:12:14 +0100

    chore: change email help note

--- resources/views/components/help-email.blade.php
+++ resources/views/components/help-email.blade.php
@@ -1,3 +1,3 @@
 <p class="my-4 text-sm text-gray-600">
-    Bei Fragen wende dich bitte an <x-link href="mailto:{{ config('mail.help_address') }}" class="underline">{{ config('mail.help_address') }}</x-link>.
+    Fragen? <x-link href="mailto:{{ config('mail.help_address') }}?subject=Konzertband Life Kontoverwaltung" class="underline">Kontakt zum Management @📧</x-link>.
 </p>
