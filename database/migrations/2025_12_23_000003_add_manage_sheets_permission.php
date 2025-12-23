776affbaf2ffcfc273a05cfdc27244692959d2a5
kolaente <k@knt.li>
2025-12-23 15:42:42 +0100

    feat: add manage sheets permission migration

---
+++ database/migrations/2025_12_23_000003_add_manage_sheets_permission.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Rights;
+use Illuminate\Database\Migrations\Migration;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        $permission = Permission::create(['name' => Rights::P_MANAGE_SHEETS]);
+        $adminRole = Role::whereName(Rights::R_ADMIN)->first();
+        $adminRole->givePermissionTo($permission);
+    }
+
+    public function down(): void
+    {
+    }
+};
