21e4230fa80036153e78694760de21a5d926c5ff
kolaente <k@knt.li>
2025-12-23 18:08:49 +0100

    feat: add NEW badge to songs and marker to set dropdown

--- resources/views/pages/sheets.blade.php
+++ resources/views/pages/sheets.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow sm:rounded-lg p-6"
-                 x-data="songSearch({{ \Illuminate\Support\Js::from($songs) }}, {{ \Illuminate\Support\Js::from($songSets->map(fn($s) => ['id' => $s->id, 'title' => $s->title])) }})">
+                 x-data="songSearch({{ \Illuminate\Support\Js::from($songs) }}, {{ \Illuminate\Support\Js::from($songSets->map(fn($s) => ['id' => $s->id, 'title' => $s->title, 'is_new' => $s->is_new])) }})">
 
                 <div class="flex flex-col sm:flex-row gap-4 mb-4">
                     <div class="flex-1">
@@ -29,7 +29,7 @@
                             >
                                 <option value="">{{ __('All Songs') }}</option>
                                 <template x-for="set in songSets" :key="set.id">
-                                    <option :value="set.id" x-text="set.title"></option>
+                                    <option :value="set.id" x-text="set.title + (set.is_new ? ' ✱' : '')"></option>
                                 </template>
                             </select>
                         </div>
@@ -38,8 +38,12 @@
 
                 <template x-for="song in results" :key="song.id">
                     <a :href="'/sheets/' + song.id"
-                       class="block py-2 hover:text-blue-600"
-                       x-text="song.title">
+                       class="block py-2 hover:text-blue-600">
+                        <span x-text="song.title"></span>
+                        <span x-show="song.is_new"
+                              class="ml-2 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-green-100 text-green-800">
+                            {{ __('NEW') }}
+                        </span>
                     </a>
                 </template>
 
