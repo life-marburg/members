bb5b1d062504dc36e1884182ecf9bd4bbe280572
kolaente <k@knt.li>
2025-12-23 17:13:11 +0100

    feat: add song search UI to sheets page

--- resources/views/pages/sheets.blade.php
+++ resources/views/pages/sheets.blade.php
@@ -7,14 +7,35 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow sm:rounded-lg p-6">
-                @forelse ($songs as $song)
-                    <a href="{{ route('sheets.show', $song) }}" class="block py-2 hover:text-blue-600">
-                        {{ $song->title }}
+            <div class="bg-white overflow-hidden shadow sm:rounded-lg p-6"
+                 x-data="songSearch({{ Js::from($songs->map(fn($s) => ['id' => $s->id, 'title' => $s->title])) }})">
+
+                <div class="mb-4">
+                    <input
+                        type="text"
+                        x-model="query"
+                        @input="search()"
+                        placeholder="{{ __('Search songs...') }}"
+                        class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                    />
+                </div>
+
+                <template x-for="song in results" :key="song.id">
+                    <a :href="'/sheets/' + song.id"
+                       class="block py-2 hover:text-blue-600"
+                       x-text="song.title">
                     </a>
-                @empty
-                    <p class="text-gray-500">{{ __('No sheets available.') }}</p>
-                @endforelse
+                </template>
+
+                <p x-show="results.length === 0 && query.trim() !== ''"
+                   class="text-gray-500">
+                    {{ __('No songs found.') }}
+                </p>
+
+                <p x-show="results.length === 0 && query.trim() === ''"
+                   class="text-gray-500">
+                    {{ __('No sheets available.') }}
+                </p>
             </div>
         </div>
     </div>
