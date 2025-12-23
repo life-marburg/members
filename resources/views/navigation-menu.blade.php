1ac9ff9016c03a383dcc19643e7f64051c5b91ef
kolaente <k@knt.li>
2025-12-23 15:52:36 +0100

    feat: render external links dynamically from database

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -21,13 +21,17 @@
                     <x-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
                         {{ __('Profile') }}
                     </x-nav-link>
-                    <x-nav-link href="https://life-marburg.de/" target="_blank">
-                        {{ __('Back to Homepage') }}
-                        <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-                            <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
-                        </svg>
-                    </x-nav-link>
+                    @foreach(\App\Models\ExternalLink::active()->ordered()->get() as $externalLink)
+                        <x-nav-link href="{{ $externalLink->url }}" target="{{ $externalLink->target }}">
+                            {{ __($externalLink->title) }}
+                            @if($externalLink->show_external_icon)
+                                <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                                    <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                                </svg>
+                            @endif
+                        </x-nav-link>
+                    @endforeach
                     @can(\App\Rights::P_MANAGE_MEMBERS)
                         <x-nav-link href="/admin" :active="request()->is('admin*')">
                             {{ __('Manage Members') }}
@@ -128,13 +132,17 @@
                 <x-responsive-nav-link href="{{ route('sheets.index') }}" :active="request()->routeIs('sheets.index') || request()->routeIs('sheets.show')">
                     {{ __('Sheets') }}
                 </x-responsive-nav-link>
-                <x-responsive-nav-link href="https://life-marburg.de/" target="_blank" class="flex items-center">
-                    {{ __('Back to Homepage') }}
-                    <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-                        <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
-                    </svg>
-                </x-responsive-nav-link>
+                @foreach(\App\Models\ExternalLink::active()->ordered()->get() as $externalLink)
+                    <x-responsive-nav-link href="{{ $externalLink->url }}" target="{{ $externalLink->target }}" class="flex items-center">
+                        {{ __($externalLink->title) }}
+                        @if($externalLink->show_external_icon)
+                            <svg xmlns="http://www.w3.org/2000/svg" class="ml-2 h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                                <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                            </svg>
+                        @endif
+                    </x-responsive-nav-link>
+                @endforeach
                 @can(\App\Rights::P_MANAGE_MEMBERS)
                     <x-responsive-nav-link href="/admin" :active="request()->is('admin*')">
                         {{ __('Manage Members') }}
