ac94ef9cea79e64d6b2271f03aa612ab1390408b
kolaente <k@knt.li>
2025-12-23 17:37:30 +0100

    chore: update captcha component to use @vite

--- resources/views/components/captcha.blade.php
+++ resources/views/components/captcha.blade.php
@@ -1,5 +1,5 @@
 @if(config('captcha.enable'))
     <div class="frc-captcha mt-4 !w-full !border-0" data-sitekey="{{ config('captcha.sitekey') }}"></div>
 
-    <script src="{{ asset('js/captcha.js') }}" async defer></script>
+    @vite('resources/js/captcha.js')
 @endif
