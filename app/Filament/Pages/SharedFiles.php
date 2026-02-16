8b668d11307028a40dc3155d3f9074009e1c5fcc
kolaente <k@knt.li>
2026-02-16 18:46:36 +0100

    feat: translate file manager UI to German
    Override all filemanager Blade views in resources/views/vendor/ with
    __() translation calls. Add German translations for all file manager
    strings (buttons, modals, empty states, tooltips, etc.) to de.json.
    Override navigation label, page title, sidebar labels and breadcrumbs
    in SharedFiles to use translated strings.

--- app/Filament/Pages/SharedFiles.php
+++ app/Filament/Pages/SharedFiles.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages;
 
+use Illuminate\Contracts\Support\Htmlable;
 use MWGuerra\FileManager\Filament\Pages\FileSystem;
 
 class SharedFiles extends FileSystem
@@ -14,5 +15,36 @@ class SharedFiles extends FileSystem
     public static function getNavigationGroup(): ?string
     {
         return null;
+    }
+
+    public static function getNavigationLabel(): string
+    {
+        return __('Files');
+    }
+
+    public function getTitle(): string|Htmlable
+    {
+        return __('Files');
+    }
+
+    public function getSidebarRootLabel(): string
+    {
+        return __('Root');
+    }
+
+    public function getSidebarHeading(): string
+    {
+        return __('Folders');
+    }
+
+    public function getBreadcrumbsProperty(): array
+    {
+        $breadcrumbs = parent::getBreadcrumbsProperty();
+
+        if (!empty($breadcrumbs) && ($breadcrumbs[0]['name'] ?? '') === 'Root') {
+            $breadcrumbs[0]['name'] = __('Root');
+        }
+
+        return $breadcrumbs;
     }
 }
